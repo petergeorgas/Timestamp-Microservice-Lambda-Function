5ae37d826958326ebf5551756268352a40cae234
Peter Georgas <peter_georgas@yahoo.com>
2022-02-20 13:39:00 -0500

    Initial implementation for AWS Lambda Function

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"time"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type TimestampEvent struct {
+	Time string `json:"time"`
+}
+
+type TimestampResponse struct {
+	UnixTime int64  `json:"unix"`
+	UTCTime  string `json:"utc"`
+}
+
+func HandleTimestampRequest(ctx context.Context, timeStamp TimestampEvent) (TimestampResponse, error) {
+	const layout = "2006-01-02"
+	parsed_time, _ := time.Parse(layout, timeStamp.Time)
+	return TimestampResponse{UnixTime: parsed_time.Unix(), UTCTime: parsed_time.UTC().String()}, nil
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	lambda.Start(HandleTimestampRequest)
 }
